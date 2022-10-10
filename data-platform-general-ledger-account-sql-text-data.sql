2039eb1d450ad4c205acdd523ada14690ddeee48
Yurino Oga <yurino.o@latona.io>
2022-10-10 13:32:46 +0900

    release

--- data-platform-general-ledger-account-sql-text-data.sql
+++ data-platform-general-ledger-account-sql-text-data.sql
@@ -7,6 +7,6 @@ CREATE TABLE `data_platform_general_ledger_account_text_data`
     `GLAccountLongName`     varchar(200) DEFAULT NULL,
     `LastChangeDateTime`    varchar(80) DEFAULT NULL,
     PRIMARY KEY (`ChartOfAccounts`, `GLAccount`, `Language`),
-    CONSTRAINT `DataPlatformGeneralLedgerAccountTextData_fk` FOREIGN KEY (`GLAccount`) REFERENCES `data_platform_general_ledger_account_chart_of_accounts_data` (`GLAccount`)
+    CONSTRAINT `DataPlatformGeneralLedgerAccountTextData_fk` FOREIGN KEY (`ChartOfAccounts`, `GLAccount`) REFERENCES `data_platform_general_ledger_account_chart_of_accounts_data` (`ChartOfAccounts`, `GLAccount`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
